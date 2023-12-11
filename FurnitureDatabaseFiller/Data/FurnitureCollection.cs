1f5472f89340f0309943bf414054633eaccd6679
пётр пашинов <yargee@bk.ru>
2023-12-11 19:33:40 +0300

    +

---
+++ FurnitureDatabaseFiller/Data/FurnitureCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureDatabaseFiller.Data
+{
+    internal class FurnitureCollection
+    {
+        public string CollectionId { get; set; }
+        public string CollectionName { get; set; }
+        public List<FurnitureElement> Elements {get;set;}
+    }
+}
